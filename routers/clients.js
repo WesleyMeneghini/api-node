996a60c9f76468c46f57c3a5b099e02b5380337a
Wesley Meneghini <54638486+WesleyMeneghini@users.noreply.github.com>
2019-12-06 18:01:50 -0200

    Colocando o metodo para atulizar e excluir

--- routers/clients.js
+++ routers/clients.js
@@ -34,7 +34,34 @@ router.post("/", (req, res) =>{
   const sql = `insert into clientes(nome) values('${nome}');`;
   conn.query(sql, (err, result) =>{
     if(err){
-      res.json(erro);
+      res.json(err);
+    }else{
+      res.json(result);
+    }
+  });
+});
+
+// deletar usuario
+router.delete("/:id", (req, res) =>{
+  const id = req.params.id;
+  const sql = `delete from clientes where codigo=${id};`; 
+  conn.query(sql, (err, result) =>{
+    if(err){
+      res.json(err);
+    }else{
+      res.json(result);
+    }
+  });
+});
+
+// atualizar usuario
+router.patch("/:id", (req, res) =>{
+  const id = req.params.id;
+  const { nome } = req.body;
+  const sql = `update clientes set nome='${nome}' where codigo=${id};`; 
+  conn.query(sql, (err, result) =>{
+    if(err){
+      res.json(err);
     }else{
       res.json(result);
     }
